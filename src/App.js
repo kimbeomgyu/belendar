e22c73e03b9fa320f8c7c8fe0cfb1c2569ddc2df
kimbeomgyu <qjarb1994@gmail.com>
2019-04-09 21:26:13 +0900

    Calendar 분리

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Calendar from './Calendar';
 
 class App extends Component {
-  day = ['월', '화', '수', '목', '금', '토', '일'];
-  table = [1, 2, 3, 4, 5, 6];
-  style = {
-    table: {
-      display: 'flex',
-      flexDirection: 'row',
-      margin: '10px'
-    },
-    el: {}
-  };
-
   render() {
     return (
       <div className="App">
-        {this.day.map(el => (
-          <div>
-            <div className="Table day" children={el} key={el} />
-            {this.table.map(el => (
-              <div className="Table" children={el} key={el} />
-            ))}
-          </div>
-        ))}
+        <Calendar />
       </div>
     );
   }
