85d6fca2c8e06e970f618c3637710d02c07fc09d
kimbeomgyu <qjarb1994@gmail.com>
2019-04-11 18:11:31 +0900

    다음달클릭함수생성 ---------------------- 함수 구분 및 현재시간버튼 위치변경

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -3,7 +3,13 @@ import { useState } from 'react';
 import './App.css';
 
 const Calendar = () => {
+  // 지정 변수들
   const day = ['월', '화', '수', '목', '금', '토', '일'];
+  const date = {
+    year: new Date().getFullYear(),
+    month: new Date().getMonth(),
+    day: new Date().getDate()
+  };
   const month = [
     'January',
     'February',
@@ -18,72 +24,66 @@ const Calendar = () => {
     'November',
     'December'
   ];
-  const date = {
-    year: new Date().getFullYear(),
-    month: new Date().getMonth(),
-    day: new Date().getDate()
-  };
 
+  // useState
   const [nowMonth, setNowMonth] = useState(date.month - 1);
   const [nowYear, setNowYear] = useState(date.year);
 
-  const dayUpdate = (year: number, month: number) => {
+  // 함수들
+  function dayUpdate(year: number, month: number) {
     const lastDay = new Date(year, month, 0).getDate();
     return new Array(35).fill(1).map((e, i) => (lastDay >= e + i ? e + i : ''));
-  };
+  }
+  function prevMonthUpdate() {
+    if (0 < nowMonth) {
+      setNowMonth(nowMonth - 1);
+    } else {
+      setNowMonth(11);
+      setNowYear(nowYear - 1);
+    }
+  }
+  function nextMonthUpdate() {
+    if (nowMonth < 11) {
+      setNowMonth(nowMonth + 1);
+    } else {
+      setNowMonth(0);
+      setNowYear(nowYear + 1);
+    }
+  }
+  function now() {
+    setNowMonth(date.month - 1);
+    setNowYear(date.year);
+  }
 
-  const title = (
+  // 년,월 표시하는 부분
+  const year = (
     <div className="month">
       <ul>
-        <li
-          className="prev"
-          children="<"
-          onClick={
-            0 < nowMonth
-              ? () => setNowMonth(nowMonth - 1)
-              : () => {
-                  setNowMonth(11);
-                  setNowYear(nowYear - 1);
-                }
-          }
-        />
+        <li className="prev" children="<" onClick={prevMonthUpdate} />
         <li className="YM">
           <span>{month[nowMonth]}</span>
           <br />
           <span>{nowYear}</span>
         </li>
-        <li
-          className="next"
-          children=">"
-          onClick={
-            nowMonth < 11
-              ? () => setNowMonth(nowMonth + 1)
-              : () => {
-                  setNowMonth(0);
-                  setNowYear(nowYear + 1);
-                }
-          }
-        />
+        <li className="next" children=">" onClick={nextMonthUpdate} />
       </ul>
     </div>
   );
 
-  return (
-    <div className="Calendar">
-      {title}
-      <button
-        className="now"
-        children="현재"
-        onClick={() => {
-          setNowMonth(date.month - 1);
-          setNowYear(date.year);
-        }}
-      />
+  // 주단위 표시하는 부분
+  const weeks = (
+    <div>
       <ul className="weeks">
         {day.map(el => (
           <li key={el} children={el} />
         ))}
       </ul>
+    </div>
+  );
+
+  // 날짜 표시하는 부분
+  const days = (
+    <div>
       <ul className="days">
         {dayUpdate(date.year, date.month).map((el, i) => (
           <li key={i + 1 + 'day'}>
@@ -91,6 +91,15 @@ const Calendar = () => {
           </li>
         ))}
       </ul>
+    </div>
+  );
+
+  return (
+    <div className="Calendar">
+      <button className="now" onClick={now} children="지금 이 순간" />
+      {year}
+      {weeks}
+      {days}
     </div>
   );
 };
