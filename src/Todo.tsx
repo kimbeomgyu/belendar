ae91ca8a7213a2b6e4b1b05b6bf0cf06eba3d6c4
kimbeomgyu <qjarb1994@gmail.com>
2019-04-15 14:27:29 +0900

    삭제버튼 추가

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -3,11 +3,7 @@ import { useState } from 'react';
 import './App.css';
 
 const Todo = () => {
-  const [todoList, setTodoList] = useState([
-    '커밋하기',
-    '일찍일어나기',
-    '매일매일 고민하기'
-  ]);
+  const [todoList, setTodoList] = useState(new Array());
   const [text, setText] = useState('');
 
   const pushText = () => {
@@ -16,15 +12,25 @@ const Todo = () => {
     setTodoList(set);
   };
 
+  const deleteText = (e: string) => {
+    const set = todoList.filter(el => {
+      return e !== el ? el : null;
+    });
+    setTodoList(set);
+  };
   return (
     <div className="todo">
       <ul>
         {todoList.map((el: string) => (
-          <li key={el + 'text'} children={el} />
+          <li key={el + 'text'}>
+            {el}
+            <button children="삭제" onClick={() => deleteText(el)} />
+          </li>
         ))}
       </ul>
       <input type="text" onChange={e => setText(e.target.value)} />
       <button onClick={pushText} children="넣기" />
+      <span style={{ color: '#f26d5b' }} children="즁복입니다" />
     </div>
   );
 };
